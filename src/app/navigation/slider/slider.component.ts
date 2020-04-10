ae793dcbabb6b41e520bddc0850289dcbbe46533
bvanh <48560384+bvanh@users.noreply.github.com>
2020-04-10 16:57:51 +0700

    change carousel

--- src/app/navigation/slider/slider.component.ts
+++ src/app/navigation/slider/slider.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ChangeDetectorRef,
+} from '@angular/core';
 import {
   SwiperComponent,
   SwiperDirective,
@@ -6,80 +12,58 @@ import {
   SwiperScrollbarInterface,
   SwiperPaginationInterface,
 } from 'ngx-swiper-wrapper';
-
+import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent {
-  public show: boolean = true;
-
-  public slides = [
+export class SliderComponent implements AfterViewInit {
+  slideNo = 0;
+  withAnim = true;
+  resetAnim = true;
+  @ViewChild('myCarousel') myCarousel: NguCarousel<any>;
+  carouselConfig: NguCarouselConfig = {
+    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+    load: 3,
+    interval: { timing: 4000, initialDelay: 1000 },
+    loop: true,
+    touch: true,
+    velocity: 0.2,
+  };
+  carouselItems = [
     {
-      title: 'Game Experience',
+      img: 'assets/slide1.png',
+      title: 'Game Exprience',
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
     {
-      title: 'Game Experience 2',
+      img: 'assets/slide1.png',
+      title: 'Game Exprience 2',
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
     {
-      title: 'Game Experience 3',
+      img: 'assets/slide1.png',
+      title: 'Game Exprience 3',
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
     {
-      title: 'Game Experience 4',
+      img: 'assets/slide1.png',
+      title: 'Game Exprience 4',
       content:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
   ];
 
-  public type: string = 'component';
-  public class: string = 'demo';
-  public disabled: boolean = false;
-  // public pagination: SwiperPaginationInterface = {
-  //   el: '.slider-pagination',
-  //   clickable: true,
-  //   hideOnClick: false,
-  //   bulletClass: 'slide-dots',
-  //   bulletActiveClass: 'slide-dots-active',
-  // };
+  constructor(private cdr: ChangeDetectorRef) {}
 
-  public config: SwiperConfigInterface = {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    // effect:"slide",
-    keyboard: true,
-    scrollbar: false,
-    navigation: true,
-    slideNextClass:"dmeo",
-    pagination: {
-      type: "bullets",
-      el: '.slide-pagination',
-      clickable: true,
-      hideOnClick: false,
-      // renderBullet: function (index, className) {
-      //   return `<span class="dot">${index}</span>`;
-      // },
-      // renderCustom: function (index, className) {
-      //   return '<div class="demo">demo' + (index + 1) + '</div>';
-      // },
-
-      // bulletClass: 'slide-dots',
-      // bulletActiveClass: 'slide-dots-active',
-      // bulletElement:`<button>demo</button>`
-    },
-    loop: true,
-  };
-  @ViewChild(SwiperComponent) componentRef: SwiperComponent;
-  @ViewChild(SwiperDirective) directiveRef: SwiperDirective;
-
-  constructor() {}
-  public onIndexChange(index: number) {
-    console.log('Swiper index: ', index);
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
+  moveTo(slide) {
+    this.myCarousel.moveTo(slide, !this.withAnim);
   }
 }
